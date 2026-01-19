9b5e5a38394fd1373bfce0d5bd74994ede11058c
chathu-02 <chathuhewamaramage@gmail.com>
2026-01-19 21:23:06 +0530

    final database and git

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { prisma } from "@/lib/prisma";
-
+export const dynamic = 'force-dynamic'
 export default async function BookingPage() {
   // Database  (Services) fetching
   const services = await prisma.service.findMany();
